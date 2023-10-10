a6316e86ca17726864b4cd9e98faa705d71fe160
Julio Chaves <chaves.julio@gmail.com>
2023-10-11 00:22:58 +0100

    Adding samples of arithmetic, relational, equality and binary operators.

--- javascript/variables/8-variables-expressions.js
+++ javascript/variables/8-variables-expressions.js
@@ -144,3 +144,123 @@ x
 x = 10 % 5 // 10 is fully divisible by 5, there is no remainder, remainder is zero
 
 x
+
+// Relational operators
+
+let comparison = 5 > 2
+
+comparison
+
+comparison = 5 < 2
+
+comparison
+
+comparison = 5 >= 5
+
+comparison
+
+comparison = 5 >= 7
+
+comparison
+
+comparison = 5 <= 5
+
+comparison
+
+comparison = 5 <= 3
+
+comparison
+
+let typeCheck = new String("Hello, am I a String?") instanceof String
+
+typeCheck
+
+typeCheck = "Hello, am I also a String?" instanceof String
+
+typeCheck
+
+// Literal types are not seen as instances of object types
+// Literals are more like primitive types, and they can be checked with typeof
+let someString = "Hello, what am I?"
+
+console.log(typeof someString)
+
+console.log(typeof someString == "string")
+
+// A literal string is not an instance of String
+console.log(someString instanceof String)
+
+typeCheck = 5 instanceof Number
+
+typeCheck
+
+// in operator
+let dog = { name: "Max" }
+
+console.log("name" in dog)
+
+// Equality operators
+
+console.log(1 == 1)
+console.log("hello" == "hello")
+console.log("1" == 1)
+console.log(0 == false)
+
+console.log(1 === 1)
+console.log("hello" === "hello")
+console.log("1" === 1)
+console.log(0 === false)
+
+console.log(1 != 1)
+console.log("hello" != "hello")
+console.log("1" != 1)
+console.log(0 != false)
+
+console.log(1 !== 1)
+console.log("hello" !== "hello")
+console.log("1" !== 1)
+console.log(0 !== false)
+
+// Binary logical operators
+
+let truthinessCheck = true && true
+
+truthinessCheck
+
+truthinessCheck = true && false
+
+truthinessCheck
+
+truthinessCheck = false && true
+
+truthinessCheck
+
+truthinessCheck = false && false
+
+truthinessCheck
+
+truthinessCheck = true || true
+
+truthinessCheck
+
+truthinessCheck = true || false
+
+truthinessCheck
+
+truthinessCheck = false || true
+
+truthinessCheck
+
+truthinessCheck = false || false
+
+truthinessCheck
+
+// Nullish coalescing operator
+
+let valueOrDefault = null ?? "Greetings"
+
+valueOrDefault
+
+valueOrDefault = "Hello" ?? "Greetings"
+
+valueOrDefault

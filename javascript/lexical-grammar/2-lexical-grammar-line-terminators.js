3b0f26fcb4f3f7d2010e248bb8649859f2fb15f2
Julio Chaves <chaves.julio@gmail.com>
2023-10-11 00:49:58 +0100

    Adding lexical grammar line terminator examples.

---
+++ javascript/lexical-grammar/2-lexical-grammar-line-terminators.js
@@ -0,0 +1,20 @@
+// This function does not provide line terminators such as
+// a carriage return \r or line feed \n
+// Without the line terminators, it reports a syntax error.
+function main() {
+    const message = "Hello world!!!" return message
+}
+
+// Another option is to use semi-colon to explicitly indicate the end of each statement.
+
+function anotherMain() {
+  const message = "Hello world!!!";return message;
+}
+
+const resultMain = main()
+
+const resultAnotherMain = anotherMain()
+
+resultMain
+
+resultAnotherMain

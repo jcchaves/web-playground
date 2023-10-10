560ea57283ba04bc7710fb88b47148b6e63727c3
Julio Chaves <chaves.julio@gmail.com>
2023-10-11 00:35:08 +0100

    Adding literal grammar spaces examples.

---
+++ javascript/lexical-grammar/1-literal-grammar-spaces.js
@@ -0,0 +1,13 @@
+// This function has horizontal spaces between operators and vertical spaces between statements
+function main() {
+    
+const location =             "main function"
+    
+    const message    = `Hello from ${location}!!!`
+
+  return       message
+}
+
+let result = main()
+
+result
